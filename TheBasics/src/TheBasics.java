fc7486818410cbea3234e6158b278c87b2c314ff
slcrowell <slcrowell3@gmail.com>
2018-05-17 14:32:24 -0400

    test

---
+++ TheBasics/src/TheBasics.java
@@ -0,0 +1,12 @@
+/**
+ * The first assignment of our summer coding adventure. This will get you used to function return
+ * values and print statements.
+ * 
+ * @author Stephen Crowell
+ *
+ */
+public class TheBasics {
+	public static int test() {
+		return 1;
+	}
+}
---
+++ TheBasics/src/TheBasicsTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TheBasicsTest {
+	
+	@Test
+	public void testTest() {
+		assertEquals(1, TheBasics.test());
+	}
+}
