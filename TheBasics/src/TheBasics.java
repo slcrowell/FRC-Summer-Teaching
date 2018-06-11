3ca77689d5c3d71c4a115b4ceea12504a541ac81
Stephen Crowell <crowelsl@rose-hulman.edu>
2018-06-11 12:26:51 -0400

    Initial commit

--- TheBasics/src/TheBasics.java
+++ TheBasics/src/TheBasics.java
@@ -2,16 +2,48 @@
  * The first assignment of our summer coding adventure. This will get you used to function return
  * values and print statements.
  * 
- * @author Stephen Crowell
+ * @author Stephen Crowell and <<YOU>>
  *
  */
 public class TheBasics {
 	
 	/**
-	 * Marie, make this function return 1.
-	 * @return 1
+	 * Check the test file to see what you need to fix this one. Don't edit the test file, change
+	 * the function.
 	 */
 	public static int test() {
-		
+		//TODO this one 
+		return 0;
+	}
+	
+	/**
+	 * The function practices using System.out.println() with strings. You want to print first, a
+	 * space, second, and a period.
+	 * @param first
+	 * @param second
+	 */
+	public static void printLinePractice(String first, String second) {
+		//TODO this one.
+	}
+	
+	/**
+	 * The function practices using System.out.println() with strings. You want to print first, a
+	 * space, second, and a period. For this one, you will need a new line character.
+	 * @param first
+	 * @param second
+	 */
+	public static void printfPractice(String first, String second) {
+		//TODO this one.
+	}
+	
+	/**
+	 * The function compares two integers. If one is greater than two, it returns 1. If two is
+	 * greater, it returns -1. If one equals two, return 0. Do not use any built in methods.
+	 * @param first
+	 * @param second
+	 */
+	public static int compare(int one, int two) {
+		//TODO this one.
+		return 0;
 	}
 }
--- TheBasics/src/TheBasicsTest.java
+++ TheBasics/src/TheBasicsTest.java
@@ -2,10 +2,40 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+/**
+ * I made this all by my self.
+ * @author Stephen Crowell
+ *
+ */
 public class TheBasicsTest {
 	
 	@Test
 	public void testTest() {
+		// This is one of the few you will fix a test. This is more for you to see how tests
+		// actually work. See if you can figure out what to do.
 		assertEquals(1, TheBasics.test());
+	}
+	
+	@Test
+	public void testStrings() {
+		System.out.println("PRINT LINE TESTS:");
+		TheBasics.printLinePractice("This is", "a test");
+		TheBasics.printLinePractice("If this isn't", "the same as that");
+		TheBasics.printLinePractice("Then your code", "isn't right");
+		
+		System.out.println("\nPRINTF TESTS");
+		TheBasics.printfPractice("This is", "a test");
+		TheBasics.printfPractice("If this isn't", "the same as that");
+		TheBasics.printfPractice("Then your code", "isn't right");
+	}
+	
+	@Test
+	public void testCompare() {
+		assertEquals(1, TheBasics.compare(2, 1));
+		assertEquals(0, TheBasics.compare(1, 1));
+		assertEquals(-1, TheBasics.compare(0, 1));
+		assertEquals(1, TheBasics.compare(-1, -2));
+		assertEquals(0, TheBasics.compare(-1, -1));
+		assertEquals(-1, TheBasics.compare(-1, 0));
 	}
 }
